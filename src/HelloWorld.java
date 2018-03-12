8bb167adbeb7ce6b0789314fcc7532ceb3b4234c
천랩4 <4@172.30.0.95>
2018-03-12 19:01:53 +0900

    HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("오랜만이야");
+		System.out.println("Hello");
+		System.out.println("World");
+		System.out.println("HelloWorld");
+	}
+	
+}
